74bda8236cd443cd0aaaa0800f512ff75cb22cd5
daviddalbusco <david.dalbusco@outlook.com>
2017-04-21 13:17:02 +0200

    Fork swing.js and angular2-swing

---
+++ src/angular2-swing/swing-stack-component.ts
@@ -0,0 +1,78 @@
+declare var require: any;
+
+import {Component, Input,
+    AfterContentInit, EventEmitter } from '@angular/core';
+
+import { Direction, ThrowEvent, DragEvent, Stack, Card, StackConfig } from './swing';
+import {SwingCardComponent} from './swing-card-component';
+
+const Swing = require('swing');
+
+@Component({
+    selector: '[swing-stack]',
+    template: `
+        <ng-content></ng-content>
+    `,
+    outputs: [
+        'throwout',
+        'throwoutend',
+        'throwoutleft',
+        'throwoutright',
+        'throwoutup',
+        'throwoutdown',
+        'throwin',
+        'throwinend',
+        'dragstart',
+        'dragmove',
+        'dragend',
+    ]
+})
+export class SwingStackComponent implements AfterContentInit {
+
+    @Input() stackConfig: StackConfig;
+
+    throwout: EventEmitter<ThrowEvent> = new EventEmitter<ThrowEvent>();
+    throwoutend: EventEmitter<ThrowEvent> = new EventEmitter<ThrowEvent>();
+    throwoutleft: EventEmitter<ThrowEvent> = new EventEmitter<ThrowEvent>();
+    throwoutright: EventEmitter<ThrowEvent> = new EventEmitter<ThrowEvent>();
+    throwoutup: EventEmitter<ThrowEvent> = new EventEmitter<ThrowEvent>();
+    throwoutdown: EventEmitter<ThrowEvent> = new EventEmitter<ThrowEvent>();
+    throwin: EventEmitter<ThrowEvent> = new EventEmitter<ThrowEvent>();
+    throwinend: EventEmitter<ThrowEvent> = new EventEmitter<ThrowEvent>();
+
+    dragstart: EventEmitter<DragEvent> = new EventEmitter<DragEvent>();
+    dragmove: EventEmitter<DragEvent> = new EventEmitter<DragEvent>();
+    dragend: EventEmitter<DragEvent> = new EventEmitter<DragEvent>();
+
+    cards: SwingCardComponent[];
+    stack: Stack;
+
+    constructor() {
+        this.cards = [];
+    }
+
+    addCard(card: SwingCardComponent) {
+        this.cards.push(card);
+        if (this.stack) {
+            this.stack.createCard(card.getNativeElement());
+        }
+    }
+
+    ngAfterContentInit() {
+        this.stack = Swing.Stack(this.stackConfig || {});
+        this.cards.forEach((c) => this.stack.createCard(c.getNativeElement()));
+
+        // Hook various events
+        this.stack.on('throwout', $event => this.throwout.emit($event));
+        this.stack.on('throwoutend', $event => this.throwoutend.emit($event));
+        this.stack.on('throwoutleft', $event => this.throwoutleft.emit($event));
+        this.stack.on('throwoutright', $event => this.throwoutright.emit($event));
+        this.stack.on('throwin', $event => this.throwin.emit($event));
+        this.stack.on('throwinend', $event => this.throwinend.emit($event));
+        this.stack.on('dragstart', $event => this.dragstart.emit($event));
+        this.stack.on('dragmove', $event => this.dragmove.emit($event));
+        this.stack.on('dragend', $event => this.dragend.emit($event));
+        this.stack.on('throwoutup', $event => this.throwoutup.emit($event));
+        this.stack.on('throwoutdown', $event => this.throwoutdown.emit($event));
+    }
+}
---
+++ src/swing/stack.js
@@ -0,0 +1,125 @@
+import _ from 'lodash';
+import Sister from 'sister';
+import rebound from 'rebound';
+import Card from './Card';
+
+/**
+ * @param {Object} config Stack configuration.
+ * @returns {Object} An instance of Stack object.
+ */
+const Stack = (config) => {
+    let eventEmitter;
+    let index;
+    let springSystem;
+    let stack;
+
+    const construct = () => {
+        stack = {};
+        springSystem = new rebound.SpringSystem();
+        eventEmitter = Sister();
+        index = [];
+    };
+
+    construct();
+
+    /**
+     * Get the configuration object.
+     *
+     * @returns {Object}
+     */
+    stack.getConfig = () => {
+        return config;
+    };
+
+    /**
+     * Get a singleton instance of the SpringSystem physics engine.
+     *
+     * @returns {Sister}
+     */
+    stack.getSpringSystem = () => {
+        return springSystem;
+    };
+
+    /**
+     * Proxy to the instance of the event emitter.
+     *
+     * @param {string} eventName
+     * @param {string} listener
+     * @returns {undefined}
+     */
+    stack.on = (eventName, listener) => {
+        eventEmitter.on(eventName, listener);
+    };
+
+    /**
+     * Creates an instance of Card and associates it with an element.
+     *
+     * @param {HTMLElement} element
+     * @param {boolean} prepend
+     * @returns {Card}
+     */
+    stack.createCard = (element, prepend) => {
+        const card = Card(stack, element, prepend);
+        const events = [
+            'throwout',
+            'throwoutend',
+            'throwoutleft',
+            'throwoutright',
+            'throwoutup',
+            'throwoutdown',
+            'throwin',
+            'throwinend',
+            'dragstart',
+            'dragmove',
+            'dragend'
+        ];
+
+        // Proxy Card events to the Stack.
+        events.forEach((eventName) => {
+            card.on(eventName, (data) => {
+                eventEmitter.trigger(eventName, data);
+            });
+        });
+
+        index.push({
+            card,
+            element
+        });
+
+        return card;
+    };
+
+    /**
+     * Returns an instance of Card associated with an element.
+     *
+     * @param {HTMLElement} element
+     * @returns {Card|null}
+     */
+    stack.getCard = (element) => {
+        const group = _.find(index, {
+            element
+        });
+
+        if (group) {
+            return group.card;
+        }
+
+        return null;
+    };
+
+    /**
+     * Remove an instance of Card from the stack index.
+     *
+     * @param {Card} card
+     * @returns {null}
+     */
+    stack.destroyCard = (card) => {
+        return _.remove(index, {
+            card
+        });
+    };
+
+    return stack;
+};
+
+export default Stack;
